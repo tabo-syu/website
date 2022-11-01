129f7c08296f2d26e6ecd54f5b9d1a30b1e512a1
tabo-syu <takeshima.nitkc@gmail.com>
2022-11-01 22:06:34 +0900

    [chore] notosansjp フォントを導入

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,25 @@
+import { Noto_Sans_JP } from '@next/font/google';
 import React from 'react';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
 import '../styles/globals.css';
+
+const noto = Noto_Sans_JP({
+  weight: ['400', '700'],
+  subsets: ['japanese'],
+  preload: true,
+});
 
 type Props = {
   children: React.ReactNode;
 };
 const Layout: React.FC<Props> = (props) => {
   return (
-    <>
+    <html lang='ja' className={noto.className}>
       <Header />
       <main>{props.children}</main>
       <Footer />
-    </>
+    </html>
   );
 };
 
