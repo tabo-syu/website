a7c281c89870772395c822e793c10de87a1a39f5
tabo-syu <takeshima.nitkc@gmail.com>
2022-11-29 08:22:01 +0900

    [fix] リストのスタイルを調整

--- components/ui/List.tsx
+++ components/ui/List.tsx
@@ -3,11 +3,11 @@ type Props = {
 };
 
 const Ul: React.FC<Props> = ({ children }) => {
-  return <ul className='mb-8 list-inside list-disc'>{children}</ul>;
+  return <ul className='mb-8 list-disc pl-6'>{children}</ul>;
 };
 
 const Ol: React.FC<Props> = ({ children }) => {
-  return <ol className='mb-8 list-inside list-decimal'>{children}</ol>;
+  return <ol className='mb-8 list-decimal pl-6'>{children}</ol>;
 };
 
 const Li: React.FC<Props> = ({ children }) => {
