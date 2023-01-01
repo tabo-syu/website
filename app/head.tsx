ef2dd13c1196458b5f94330f17326b22c6aadb0d
tabo-syu <takeshima.nitkc@gmail.com>
2023-01-01 23:42:24 +0900

    [feat] GTM の導入

--- app/head.tsx
+++ app/head.tsx
@@ -1,12 +1,21 @@
+import GoogleTagManager, {
+  GoogleTagManagerId,
+} from '@/components/ui/GoogleTagManager';
 import TopHead from '@/components/ui/Head';
+import { googleTagManagerId } from '@/lib/gtm';
 
 export default function Head() {
   return (
-    <TopHead
-      title=''
-      description='たぼすけのブログ。いろいろなことを書く（予定）。'
-      image='/ogp.jpg'
-      type='website'
-    />
+    <>
+      <TopHead
+        title=''
+        description='たぼすけのブログ。いろいろなことを書く（予定）。'
+        image='/ogp.jpg'
+        type='website'
+      />
+      <GoogleTagManager
+        googleTagManagerId={googleTagManagerId as GoogleTagManagerId}
+      />
+    </>
   );
 }
